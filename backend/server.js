55581707a75d96712da83b9e39ed2c2444ed547b
Sintja <sporeci2000@gmail.com>
2025-09-09 20:02:34 -0400

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Get our server running
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
