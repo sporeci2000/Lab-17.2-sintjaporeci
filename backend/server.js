94751523251f999876b69415a7e53cdc80073c14
Sintja <sporeci2000@gmail.com>
2025-09-10 19:15:02 -0400

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 require('./config/db-connection');
 const express = require('express');
+const cors = require('cors');
 const noteRouter = require('./routes/notes-router')
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/notes, noteRouter');
 
 // Get our server running
